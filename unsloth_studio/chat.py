4281492ae62c1f858fd22da38da933ae227bd387
Manav Majumdar <kookygavinquack@gmail.com>
2025-02-16 14:19:46 -0500

    Update chat.py for UIGEN

--- unsloth_studio/chat.py
+++ unsloth_studio/chat.py
@@ -19,7 +19,7 @@ import subprocess
 import os
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
-MODEL_NAME = "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit"
+MODEL_NAME = "smirki/UIGEN-7B-16bit"
 
 print("Installing packages for 🦥 Unsloth Studio ... Please wait 5 minutes ...")
 
